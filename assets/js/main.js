d69ec777409910a56f0a22febaad7f61a0ce25b6
luna <novitaanggraini_5235151423@mhs.unj.ac.id>
2022-12-15 20:51:24 +0700

    idk what is dis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
 (function () {
+  /*window.addEventListener('load', () => {
+    Swal.fire({
+      //title: 'Do you want to play music in the background?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        document.querySelector('.song').play();
+        animationTimeline();
+      } else {
+        animationTimeline();
+      }
+    });
+  });*/
+
     var on = addEventListener,
       $ = function (q) {
         return document.querySelector(q)
